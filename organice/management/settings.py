beaa4bc02ca0047633f467202d5c03cf19c7daa0
Peter Bittner <django@bittner.it>
2014-02-22 17:52:13 +0100

    Added DjangoModuleManager class, refactored urls configuration

--- organice/management/settings.py
+++ organice/management/settings.py
@@ -19,37 +19,35 @@ import os
 import re
 
 
-class DjangoSettingsManager(object):
+class DjangoModuleManager(object):
     """
-    Utility class which allows moving and copying variables in-between several
-    settings files in the project's ``settings/`` folder.
+    Utility class to modify and write files in a Python module.
     """
     __path = ''
     __file = {}
     __data = {}
-    NO_MATCH = (0, 0)
 
-    def __init__(self, projectname, *filenames):
-        """
-        Constructor to add settings files (named without path and extension).
-        """
-        self.__path = os.path.join(projectname, 'settings')
+    def __init__(self, projectname, *modulename):
+        """Constructor, computes and creates physical base path for module"""
+        self.__path = os.path.join(projectname, *modulename)
         if not os.path.exists(self.__path):
             os.makedirs(self.__path)
-        for module in filenames:
-            self.add_file(module)
 
-    def add_file(self, module):
+    def add_file(self, module, data=None, lines=None):
         """
-        Adds a settings file (identified by its module name).
+        Adds a Python file (identified by its module name) in the module.
         If the related .py file doesn't exist an empty file is created.
         """
         file = open(os.path.join(self.__path, module + '.py'), 'a+')
         self.__file[module] = file
-        self.__data[module] = file.read()
+        self.__data[module] = '' if data or lines else file.read()
+        if data:
+            self.set_data(module, data)
+        if lines:
+            self.append_lines(module, *lines)
 
     def get_file(self, module):
-        """Returns the file object for a settings module"""
+        """Returns the file object for a module file"""
         return self.__file[module]
 
     def save_files(self):
@@ -60,13 +58,46 @@ class DjangoSettingsManager(object):
             file.truncate()
             file.write(data)
 
+    def get_data(self, module):
+        """Returns the data contained in the module file"""
+        return self.__data[module]
+
+    def set_data(self, module, data):
+        """Sets the data contained in the module file"""
+        self.__data[module] = data
+
+    def append_data(self, module, chunk):
+        """Appends a chunk of data to the module file"""
+        self.__data[module] += chunk
+
+    def append_lines(self, module, *lines):
+        """Appends lines of text to the module file"""
+        if len(self.__data[module]) > 0:
+            self.append_data(module, os.linesep)
+        for data in lines:
+            self.append_data(module, data + os.linesep)
+
+
+class DjangoSettingsManager(DjangoModuleManager):
+    """
+    Utility class which allows moving and copying variables in-between several
+    settings files in the project's ``settings/`` folder.
+    """
+    NO_MATCH = (0, 0)
+
+    def __init__(self, projectname, *filenames):
+        """Constructor, adds settings files (named without path and extension)"""
+        DjangoModuleManager.__init__(self, projectname, 'settings')
+        for module in filenames:
+            DjangoModuleManager.add_file(self, module)
+
     def find_var(self, src, var, comments=True):
         """
         Returns (start, stop) position of a match, or NO_MATCH i.e. (0, 0).
         A match is a variable including optional leading comment lines.  If
         comments is set to False the match strictly starts with the variable.
         """
-        data = self.__data[src]
+        data = DjangoModuleManager.get_data(self, src)
 
         # variable incl. leading comments, until after trailing equal sign
         # and optional line continuation mark (backslash)
@@ -124,23 +155,14 @@ class DjangoSettingsManager(object):
             ignore, stop = m.span()
         return stop
 
-    def __append(self, dest, chunk):
-        self.__data[dest] += chunk
-
     def __insert(self, dest, start, stop, chunk):
-        data = self.__data[dest]
-        self.__data[dest] = data[:start] + chunk + data[stop:]
-
-    def append_lines(self, dest, *lines):
-        if len(self.__data[dest]) > 0:
-            self.__append(dest, os.linesep)
-        for data in lines:
-            self.__append(dest, data + os.linesep)
+        data = DjangoModuleManager.get_data(self, dest)
+        DjangoModuleManager.set_data(self, dest, data[:start] + chunk + data[stop:])
 
     def insert_lines(self, dest, *lines):
         """Finds position after first comment and inserts the data"""
         pattern = re.compile(r'(\s*#.*\n)*')
-        match = pattern.search(self.__data[dest])
+        match = pattern.search(DjangoModuleManager.get_data(self, dest))
         start, stop = self.NO_MATCH if match is None else match.span()
         chunk = ''
         for data in lines:
@@ -159,22 +181,18 @@ class DjangoSettingsManager(object):
 
     def delete_var(self, dest, var):
         """Deletes a variable from a settings file"""
-        data = self.__data[dest]
         start, stop = self.find_var(dest, var)
-        self.__data[dest] = data[:start] + data[stop:]
+        data = DjangoModuleManager.get_data(self, dest)
+        DjangoModuleManager.set_data(self, dest, data[:start] + data[stop:])
 
     def copy_var(self, src, destinations, var):
-        """
-        Copies a variable from one settings file to one or more others.
-        """
+        """Copies a variable from one settings file to one or more others"""
         start, stop = self.find_var(src, var)
-        data = self.__data[src][start:stop]
+        data = DjangoModuleManager.get_data(self, src)[start:stop]
         for dest in destinations:
-            self.__append(dest, data)
+            DjangoModuleManager.append_data(self, dest, data)
 
     def move_var(self, src, destinations, var):
-        """
-        Moves a variable from one settings file to one or more others.
-        """
+        """Moves a variable from one settings file to one or more others"""
         self.copy_var(src, destinations, var)
         self.delete_var(src, var)
