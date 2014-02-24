ce7a7549145ca3fc82b53b2a7c2febd6b220f753
Peter Bittner <django@bittner.it>
2014-02-24 19:23:45 +0100

    Added South migrations for Zinnia EntryPlaceholder - as discussed in https://github.com/Fantomas42/django-blog-zinnia/issues/115#issuecomment-35863104

---
+++ organice/migrations/__init__.py
@@ -0,0 +1,5 @@
+"""South migrations for django Organice"""
+
+# In settings/common.py: SOUTH_MIGRATION_MODULES = {'zinnia': 'organice.migrations.zinnia',}
+# $ ./manage.py schemamigration zinnia --initial
+# $ ./manage.py schemamigration zinnia --auto entry_placeholder
