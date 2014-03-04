19e1156d27e70750441b4f2f5a3a7b9f806913f6
Peter Bittner <django@bittner.it>
2014-03-04 15:18:59 +0100

    Moved html5shiv inclusion to head (template issue #2)

--- organice/__init__.py
+++ organice/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1'
+__version__ = '0.2.dev'
 __revision__ = '<<--git-commit-hash-->>'
 
 __author__ = 'Peter Bittner'
