6b40ceabccd2c52e196bd284fa7f4076af7d8374
Peter Bittner <django@bittner.it>
2014-03-26 22:57:31 +0100

    Upgraded to bootstrap-sass v3.1.1

--- Makefile
+++ Makefile
@@ -36,7 +36,7 @@ bumpver:
 
 clean:
 	$(MAKE) -C docs clean
-	rm -rf build/ dist/ django_organice.egg-info/ docs/build/ docs/locale/
+	rm -rf build/ dist/ django_organice.egg-info/ docs/build/ docs/locale/ organice/static/.sass-cache
 	for DIR in media/ static/ templates/ ; do \
 		[ -d $$DIR ] && rmdir $$DIR || true ; \
 	done
--- organice/static/config.rb
+++ organice/static/config.rb
@@ -1,11 +1,13 @@
+require 'bootstrap-sass'
 # Additional compass plugins
-require 'susy'
+
 # Project directories
+http_path = "/static/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 # CSS generation
 output_style = :compressed
-line_comments = false
 relative_assets = true
+line_comments = false
