233900ee876033f19e239e2b9f75e5cf55285334
Peter Bittner <django@bittner.it>
2016-08-29 15:10:58 +0200

    Remove migration not relevant for Zinnia 0.16

--- organice/migrations/zinnia/0004_entry_content_placeholder.py
+++ organice/migrations/zinnia/0004_entry_content_placeholder.py
@@ -9,13 +9,13 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('cms', '0016_auto_20160608_1535'),
-        ('zinnia', '0004_on_delete_arg'),
+        ('zinnia', '0003_publication_date'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='entry',
             name='content_placeholder',
-            field=cms.models.fields.PlaceholderField(to='cms.Placeholder', null=True, editable=False, slotname='content'),
+            field=cms.models.fields.PlaceholderField(editable=False, to='cms.Placeholder', slotname='content', null=True),
         ),
     ]
